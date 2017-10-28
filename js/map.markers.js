898b4954ef6dc2f5b4376999090b1b19292c9edb
Andrew Liston <aliston18@icloud.com>
2017-10-28 14:36:19 -0400

    changed map markers

--- js/map.markers.js
+++ js/map.markers.js
@@ -2,44 +2,16 @@
 var map_markers = [
 	{
 		"title": "Reception",
-		"latitude":33.775664,
-		"longitude":-118.065090,
+		"latitude":45.1510532655634,
+		"longitude":-79.398193359375,
 		"icon":"fa-glass", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Old Ranch Country Club <br> 29033 West Lake Drive, Agoura Hills, CA"
-	},
-	{
-		"title": "Accommodation 1",
-		"latitude":33.777929,
-		"longitude":-118.076891,
-		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Accommodation 1 <br> Rossmoor, CA 90720"
-	},
-	{
-		"title": "Accommodation 2",
-		"latitude":33.780428,
-		"longitude":-118.084075,
-		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Accommodation 2 <br> Los Alamitos, CA 90720"
-	},
-	{
-		"title": "Accommodation 3",
-		"latitude":33.777551,
-		"longitude":-118.050468,
-		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Accommodation 3 <br> Seal Beach, CA 90740"
-	},
-	{
-		"title": "Transportation",
-		"latitude":33.782930,
-		"longitude":-118.060552,
-		"icon":"fa-plane", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Seal Beach VORTAC SLI <br> Los Alamitos, CA 90720"
+		"infoWindow":"Walter and Joanne's Backyard <br> 4 Rue du Long-Sault, Gatineau, QC J9H 1G2"
 	},
 	{
 		"title": "Ceremony",
-		"latitude":33.776384,
-		"longitude":-118.058149,
+		"latitude":45.1510532655634,
+		"longitude":-79.398193359375,
 		"icon":"fa-bell-o", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Birchwood Church <br> Seal Beach, CA 90740"
+		"infoWindow":"Rockcliffe Park <br> 1495 Sir George-Etienne Cartier Pky, Ottawa, ON K1K 4Y5"
 	}
 ];
